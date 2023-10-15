99d8ed9ce9a7fa12b1aed8042e9025ce3702a3db
trl0rd <varun110301@gmail.com>
2023-10-15 23:18:13 +0530

    Added the background to the site also addedn the bio and about me section

---
+++ portfolio/components/section-heaading.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type sectionHeadingProps = {
+  children: React.ReactNode;
+};
+
+export default function SectionHeading({ children }: sectionHeadingProps) {
+  return <h2 className="text-3xl font-medium capitalize mb-8">{children}</h2>;
+}
